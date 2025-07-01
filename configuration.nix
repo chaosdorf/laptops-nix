ae4ce23165eb2606b87a480902581a3d73332d2f
Niklas Sombert <niklas@ytvwld.de>
2025-07-01 18:58:26 +0200

    Reinstall with NixOS 25.05

--- configuration.nix
+++ configuration.nix
@@ -6,6 +6,7 @@
 
 {
   imports = [
+    ./hardware-configuration.nix
   ];
 
   # Bootloader.
@@ -81,7 +82,8 @@
   environment.systemPackages = with pkgs; [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
-     kdePackages.discover
+    git
+    kdePackages.discover
   ];
 
   # List services that you want to enable:
@@ -97,5 +99,5 @@
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.05"; # Did you read the comment?
+  system.stateVersion = "25.05"; # Did you read the comment?
 }
