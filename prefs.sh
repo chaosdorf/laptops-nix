3e8f342d6f1253c62680ddec06148a549f3e16ef
Niklas Sombert <niklas@ytvwld.de>
2025-10-01 20:27:37 +0200

    Add guest users to the systemd-journal group

--- prefs.sh
+++ prefs.sh
@@ -2,3 +2,5 @@
 
 # set the password, so that the user can unlock their screen
 echo "guest" | passwd $USER -s
+# add the user to the systemd-journal group, so that they can run `journalctl`
+usermod --groups systemd-journal --append $USER
