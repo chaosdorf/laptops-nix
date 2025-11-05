21391a2e1dc922a5aecf6d2f62fe9f2c7037173b
Niklas Sombert <niklas@ytvwld.de>
2025-11-05 19:12:30 +0100

    Add ws2

---
+++ ws2.nix
@@ -0,0 +1,31 @@
+{ config, lib, pkgs, modulesPath, ... }:
+
+{
+  imports =
+    [
+      ./common.nix
+      (modulesPath + "/installer/scan/not-detected.nix")
+    ];
+  
+  networking.hostName = "ws2"; # Define your hostname.
+  boot.plymouth.theme = "cerealkiller";
+
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ehci_pci" "ahci" "usb_storage" "sd_mod" "sdhci_pci" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-intel" ];
+  boot.extraModulePackages = [ ];
+
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/43927392-0be3-4970-a070-df0346e2f420";
+      fsType = "btrfs";
+      options = [ "subvol=@" ];
+    };
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/0336-0092";
+      fsType = "vfat";
+      options = [ "fmask=0077" "dmask=0077" ];
+    };
+
+  swapDevices = [ ];
+}
